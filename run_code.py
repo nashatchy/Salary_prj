fc0d2908bc1d33e7b72a0b7517ecdbf5d6a8c3e5
nashatchy <64709575+nashatchy@users.noreply.github.com>
2020-05-02 20:14:40 -0400

    uploading data and code

--- run_code.py
+++ run_code.py
@@ -3,4 +3,6 @@
 import glassdoor_scrapper as gs 
 import pandas as pd 
 path="D:/Github/Salary_prj"
-df= gs.get_jobs('data scientist',10, False, path, 5)
+df= gs.get_jobs('data scientist',1000, False, path, 10)
+
+df.to_csv('glassdoor_scrapper_result',index=False)
